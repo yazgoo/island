18db81c0ef294ff7ce5f84399f51af0ac0235646
yazgoo <yazgoo@gmail.com>
2017-07-12 09:22:39 +0200

    add exit

--- island.rb
+++ island.rb
@@ -12,25 +12,14 @@ class IslandWindow < Gosu::Window
     CP::Vec2.new a, b
   end
 
-  def initialize_physics
-    @space = CP::Space.new
-    @space.damping = 0.8
-    @space.gravity = v2(0, 1000)
-    # infinite moment of inertia to make body non rotatable
-    character_body = CP::Body.new(10, CP::INFINITY)
-    w = @character_noframes.width * 0.25
-    h = @character_noframes.height * 0.25
-    @limits = [[0, 0], [0, h], [w, h], [w, 0]].map { |x| v2 x[0], x[1]}
-    #v2(-25.0, -25.0), v2(-25.0, 25.0), v2(25.0, 1.0), v2(25.0, -1.0)
-    @character_shape = CP::Shape::Poly.new(character_body, @limits, v2(1000, 0))
-    @character_shape.collision_type = :character
-    @character_shape.u = 1.5
-    @space.add_body character_body
-    @space.add_shape @character_shape
+  def unmovable_body
+    CP::Body.new(CP::INFINITY, CP::INFINITY)
+  end
+
+  def initialize_platforms
     @platforms = @scene["platforms"].map do |k, v| 
       pos = v["pos"]
-      platform_body = CP::Body.new(CP::INFINITY, CP::INFINITY) 
-      platform_shape = CP::Shape::Segment.new(platform_body, v2(pos[0], pos[1]), v2(pos[2], pos[3]), 3) 
+      platform_shape = CP::Shape::Segment.new(unmovable_body, v2(pos[0], pos[1]), v2(pos[2], pos[3]), 3) 
       platform_shape.collision_type = :ground
       # friction
       platform_shape.u = 1.5
@@ -39,9 +28,68 @@ class IslandWindow < Gosu::Window
       @space.add_static_shape platform_shape
       platform_shape
     end
+  end
+
+  def v_limits w, h
+    [[0, 0], [0, h], [w, h], [w, 0]].map { |x| v2 x[0], x[1]}
+  end
+
+  def initialize_exit
+   e = @scene["exit"]
+   @exit_shape = CP::Shape::Poly.new(unmovable_body, v_limits(e[2], e[3]), v2(e[0], e[1]))
+   @exit_shape.collision_type = :exit
+   @space.add_static_shape @exit_shape
+  end
+
+  def delete_scene
+    @space.remove_shape @exit_shape
+    @platforms.each { |platform| @space.remove_shape @platform }
+    @decorations = []
+  end
+
+  def initialize_physics
+    @space = CP::Space.new
+    @space.damping = 0.8
+    @space.gravity = v2(0, 1000)
+    # infinite moment of inertia to make body non rotatable
+    character_body = CP::Body.new(10, CP::INFINITY)
+    w = @character_noframes.width * 0.25
+    h = @character_noframes.height * 0.25
+    @character_shape = CP::Shape::Poly.new(character_body, v_limits(w, h), v2(1000, 0))
+    @character_shape.collision_type = :character
+    @character_shape.u = 1.5
+    @space.add_body character_body
+    @space.add_shape @character_shape
     @space.add_collision_func(:character, :ground) do |character, ground|
       @touching_ground = true
     end
+    @space.add_collision_func(:character, :exit) do |character, ground|
+      @exit = true
+    end
+    @space.add_post_step_callback(1) do 
+      p "called"
+      if @exit
+        p "post"
+        delete_scene
+        initialize_scene
+      end
+    end
+  end
+
+  def initialize_decorations
+    @decorations = @scene["decorations"].map do |k, v|
+      v = {"pos"=>[0, 0]}.merge(v.nil? ? {} : v) if v.nil? or v["pos"].nil?
+      [k, {image: Gosu::Image.new("media/#{k}.png")}.merge(v)]
+    end
+  end
+
+  def initialize_scene
+    @exit = false
+    @scene = YAML.load_file('scenes.yml')["beach0"]
+    initialize_exit
+    initialize_platforms
+    @music = Gosu::Sample.new( self, "media/#{@scene["music"]}.ogg")
+    initialize_decorations
   end
 
   def initialize
@@ -50,13 +98,8 @@ class IslandWindow < Gosu::Window
     #@music.play(0.5, 1, true)
     @character_frames = Dir.glob('media/character/*.png').map { |x| Gosu::Image.new(x) }
     @character_noframes = Gosu::Image.new('media/character.png')
-    @scene = YAML.load_file('scenes.yml')["beach0"]
     initialize_physics
-    @music = Gosu::Sample.new( self, "media/#{@scene["music"]}.ogg")
-    @decorations = @scene["decorations"].map do |k, v|
-      v = {"pos"=>[0, 0]}.merge(v.nil? ? {} : v) if v.nil? or v["pos"].nil?
-      [k, {image: Gosu::Image.new("media/#{k}.png")}.merge(v)]
-    end
+    initialize_scene
     @t = 0
     @translation = 0
     @direction = 1
@@ -81,7 +124,8 @@ class IslandWindow < Gosu::Window
     @character.draw_rot(x, @character_shape.body.p.y, 0, 180, 0, 2, -0.25 * @direction, 0.25, 0x11_000000)
   end
 
-  def draw_bounding_box a
+  def draw_bounding_box shape
+    a = shape.bb
     draw_line(a.l, a.t, Gosu::Color::RED, a.r, a.t, Gosu::Color::RED)
     draw_line(a.r, a.t, Gosu::Color::RED, a.r, a.b, Gosu::Color::RED)
     draw_line(a.r, a.b, Gosu::Color::RED, a.l, a.b, Gosu::Color::RED)
@@ -104,9 +148,9 @@ class IslandWindow < Gosu::Window
     @t += 1
     draw_decorations
     draw_character
-    draw_bounding_box @character_shape.bb
-    @platforms.each { |platform| draw_bounding_box platform.bb }
-
+    draw_bounding_box @character_shape
+    draw_bounding_box @exit_shape
+    @platforms.each { |platform| draw_bounding_box platform }
   end
 
   def button_down( id )
